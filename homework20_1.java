7424ddad1ab48f9a68665c36032822af235bbdb8
rorystrohm <86379755+rorystrohm@users.noreply.github.com>
2021-06-24 15:09:05 -0400

    Update homework20_1.java

--- homework20_1.java
+++ homework20_1.java
@@ -19,14 +19,42 @@ public class homework20_1
 {
    public static void main(String[] args)
    {
-      //add your code here
-   
+      LinkedList llist = new LinkedList();
+      llist.head = new ListNode(1);
+      llist.head.next = new ListNode(2);
+      llist.head.next.next = new ListNode(3);
+      llist.head.next.next.next = new ListNode(3);
+      llist.head.next.next.next.next = new ListNode(5);
+      System.out.println("input: " + llist);
+      deleteDuplicates(llist);
+      System.out.println("output: " + llist);
+      llist = new LinkedList();
+      llist.head = new ListNode(1);
+      llist.head.next = new ListNode(1);
+      llist.head.next.next = new ListNode(1);
+      llist.head.next.next.next = new ListNode(2);
+      llist.head.next.next.next.next = new ListNode(3);
+      System.out.println("\ninput: " + llist);
+      deleteDuplicates(llist);
+      System.out.println("output: " + llist);
    }
    public static void deleteDuplicates(LinkedList llist)
    {
-      //add your code here
+      ListNode curr1 = llist.head;
+      ListNode curr2;
+      while(curr1 != null) {
+         int value = curr1.value;
+         curr2 = curr1;
+         while(curr2.next != null) {
+            if(curr2.next.value == value) {
+               curr2.next = curr2.next.next;
+            } else {
+               curr2 = curr2.next;
+            }
+         }
+         curr1 = curr1.next;
+      }
    }
-
 }
 
 class ListNode
