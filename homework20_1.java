77c154b3263dbd43d1e54e99bd0dd509b9043eb5
rorystrohm <86379755+rorystrohm@users.noreply.github.com>
2021-07-12 11:25:04 -0400

    Add files via upload

--- homework20_1.java
+++ homework20_1.java
@@ -1,61 +1,52 @@
-/*
-Given a sorted linked list, delete all nodes that have duplicate
-numbers, leaving only distinct numbers from the original list.In this 
-program, you need to 
-(1) read 5 numbers and set them in a linkedlist in order
-(2) delete duplicate
-Example 1:
-input: 1,2,3,3,5
-output: head-->1-->2-->3-->5-->null
-Example 2:
-input: 1,1,1,2,3
-output: head-->1-->2-->3-->null
-Please complete the following program to fullfil the function.
-*/
-
-import java.util.Iterator;
-
 public class homework20_1
 {
    public static void main(String[] args)
    {
-      LinkedList linklist = new LinkedList();
-      linklist.head = new ListNode(1);
-      linklist.head.next = new ListNode(2);
-      linklist.head.next.next = new ListNode(3);
-      linklist.head.next.next.next = new ListNode(3);
-      linklist.head.next.next.next.next = new ListNode(5);
-      System.out.print("/ninput: " + linklist);
-      deleteDuplicates(linklist);
-      System.out.println("output: " + linklist);
-      
-      LinkedList lunklist = new LinkedList();
-      lunklist = new LinkedList();
-      lunklist.head = new ListNode(1);
-      lunklist.head.next = new ListNode(1);
-      lunklist.head.next.next = new ListNode(1);
-      lunklist.head.next.next.next = new ListNode(2);
-      lunklist.head.next.next.next.next = new ListNode(3);
-      System.out.println("/ninput: " + lunklist);
-      deleteDuplicates(lunklist);
-      System.out.println("output: " + lunklist);
+       //creating a linked list
+       LinkedList llist = new LinkedList();
+       llist.head = new ListNode(1);
+       llist.head.next = new ListNode(2);
+       llist.head.next.next = new ListNode(3);
+       llist.head.next.next.next = new ListNode(3);
+       llist.head.next.next.next.next = new ListNode(5);
+
+       System.out.println("input: " + llist); //printing initial list
+       deleteDuplicates(llist); //calling deleteDuplicates method
+       System.out.println("output: " + llist); // printing llist
+
+       //creating a new Linked list
+       llist = new LinkedList();
+       llist.head = new ListNode(1);
+       llist.head.next = new ListNode(1);
+       llist.head.next.next = new ListNode(1);
+       llist.head.next.next.next = new ListNode(2);
+       llist.head.next.next.next.next = new ListNode(3);
+
+       System.out.println("\ninput: " + llist); //printing initial list
+       deleteDuplicates(llist); //calling deleteDuplicates method
+       System.out.println("output: " + llist); // printing llist
    }
-   public static void deleteDuplicates(LinkedList list)
+
+   public static void deleteDuplicates(LinkedList llist)
    {
-      ListNode curr1 = list.head;
-      ListNode curr2;
-      while(curr1 != null) {
-         int value = curr1.value;
-         curr2 = curr1;
-         while(curr2.next != null) {
-            if(curr2.next.value == value) {
-               curr2.next = curr2.next.next;
-            } else {
-               curr2 = curr2.next;
-            }
-         }
-         curr1 = curr1.next;
-      }
+       //two temporary ListNode references
+       ListNode curr1 = llist.head; //initialized to head of llist
+       ListNode curr2;
+       while(curr1 != null){ //runs until end of list is reached
+           int value = curr1.value; //storing value of first ListNode reference variable
+           curr2 = curr1; //second ListNode referencing first reference
+           while(curr2.next != null) //runs until Second references the second Node from the last
+           {
+               //if any duplicate found
+               if(curr2.next.value == value)
+                   //removing node with duplicated value
+                   curr2.next = curr2.next.next;
+               else // updating Second reference to next node of it
+                   curr2 = curr2.next;
+           }
+           // updating First reference to next node of it
+           curr1 = curr1.next;
+       }
    }
 }
 
@@ -71,13 +62,13 @@ class LinkedList
    ListNode head;
    public String toString()
    {
-      String nodeData = "";
-      ListNode ref = head;
-      while(ref != null)
-      {
-         nodeData += ref.value + "-->";
-         ref = ref.next;
-      }
-      return "head-->"+nodeData+"null";
+       String nodeData = "";
+       ListNode ref = head;
+       while(ref != null)
+       {
+           nodeData += ref.value + "-->";
+           ref = ref.next;
+       }
+       return "head-->"+nodeData+"null";
    }
 }
